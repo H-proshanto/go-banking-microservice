c6d7c48a4a687f1db7b8ba749bd0146e8ed3cf1f
Proshanto Lal <>
2023-08-30 00:22:38 +0600

    adds route for single customer

--- banking/cmd/app.go
+++ banking/cmd/app.go
@@ -17,6 +17,7 @@ func Start() {
 	// ch := CustomerHandlers{service: service.NewCustomerService(domain.NewCustomerRepositoryStub())}
 	ch := CustomerHandlers{service: service.NewCustomerService(domain.NewCustomerRepoDB())}
 	router.HandleFunc("/customers", ch.getAllCustomers).Methods(http.MethodGet)
+	router.HandleFunc("/customers/{customer_id:[0-9]+}", ch.getCustomer).Methods(http.MethodGet)
 
 	fmt.Printf("Server started on %v \n", "0.0.0.0:8000")
 	log.Fatal(http.ListenAndServe("0.0.0.0:8000", router))
