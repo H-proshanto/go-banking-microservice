aa3543a2f7bde8fe8a28b321d7585cf933c14170
Proshanto Lal <>
2023-08-30 00:25:50 +0600

    adds GetCustomer to primary port for customer service

--- banking/service/customerSvc.go
+++ banking/service/customerSvc.go
@@ -1,17 +1,25 @@
 package service
 
-import "github.com/H-proshanto/go-banking-microservice/banking/domain"
+import (
+	"github.com/H-proshanto/go-banking-microservice/banking/domain"
+	"github.com/H-proshanto/go-banking-microservice/banking/errs"
+)
 
 type CustomerService interface {
-	GetAllCustomers() ([]*domain.Customer, error)
+	GetAllCustomers() ([]*domain.Customer, *errs.AppError)
+	GetCustomer(id string) (*domain.Customer, *errs.AppError)
 }
 
 type DefaultCustomerService struct {
 	repo domain.CustomerRepository
 }
 
-func (s DefaultCustomerService) GetAllCustomers() ([]*domain.Customer, error) {
+func (s DefaultCustomerService) GetAllCustomers() ([]*domain.Customer, *errs.AppError) {
 	return s.repo.FindAll()
+}
+
+func (s DefaultCustomerService) GetCustomer(id string) (*domain.Customer, *errs.AppError) {
+	return s.repo.ById(id)
 }
 
 func NewCustomerService(repo domain.CustomerRepository) *DefaultCustomerService {
