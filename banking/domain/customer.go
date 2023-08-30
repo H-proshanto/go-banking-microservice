ee97998708a23c45f043aac49bf0c7cbe1e2c42f
H-proshanto <proshantolal3@gmail.com>
2023-08-31 01:44:24 +0600

    uses sqlx pkg instead of gorm for database query and connection

--- banking/domain/customer.go
+++ banking/domain/customer.go
@@ -3,15 +3,15 @@ package domain
 import "github.com/H-proshanto/go-banking-microservice/banking/errs"
 
 type Customer struct {
-	ID          string
+	ID          string `db:"customer_id"`
 	Name        string
 	City        string
 	Zipcode     string
-	DateOfBirth string
+	DateOfBirth string `db:"date_of_birth"`
 	Status      string
 }
 
-type CustomerRepository interface { 
+type CustomerRepository interface {
 	FindAll(status string) ([]*Customer, *errs.AppError)
-	ById(id string) (*Customer, *errs.AppError) 
+	ById(id string) (*Customer, *errs.AppError)
 }
--- banking/domain/customerRepoDB.go
+++ banking/domain/customerRepoDB.go
@@ -6,17 +6,17 @@ import (
 
 	"github.com/H-proshanto/go-banking-microservice/banking/errs"
 	"github.com/H-proshanto/go-banking-microservice/banking/logger"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
 )
 
 type CustomerRepoDB struct {
-	db *gorm.DB
+	db *sqlx.DB
 }
 
 func NewCustomerRepoDB() *CustomerRepoDB {
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable ", "localhost", "postgres", "password", "banking", "5432")
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err := sqlx.Connect("postgres", dsn)
 	if err != nil {
 		logger.Error(err.Error())
 		panic(err)
@@ -29,55 +29,35 @@ func NewCustomerRepoDB() *CustomerRepoDB {
 }
 
 func (r *CustomerRepoDB) FindAll(status string) ([]*Customer, *errs.AppError) {
-	var rows *sql.Rows
 	var err error
+	customers := make([]*Customer, 0)
 
 	if status == "" {
 		findAllSqlQuery := "SELECT customer_id, name, city, zipcode, date_of_birth, status from customers"
-		rows, err = r.db.Raw(findAllSqlQuery).Rows()
-
+		err = r.db.Select(&customers, findAllSqlQuery)
 	} else {
-		findAllSqlQuery := "SELECT customer_id, name, city, zipcode, date_of_birth, status from customers WHERE status = ?"
-		rows, err = r.db.Raw(findAllSqlQuery, status).Rows()
-
+		logger.Error(status)
+		findAllSqlQuery := "SELECT customer_id, name, city, zipcode, date_of_birth, status from customers WHERE status = $1"
+		err = r.db.Select(&customers, findAllSqlQuery, status)
 	}
 
 	if err != nil {
-		logger.Error("Error while querying customer table" + err.Error())
-		return nil, errs.NewUnexpectedError("something unexpected happend")
-	}
-
-	defer rows.Close()
-
-	customers := make([]*Customer, 0)
-
-	for rows.Next() {
-		var c Customer
-		err := rows.Scan(&c.ID, &c.Name, &c.City, &c.Zipcode, &c.DateOfBirth, &c.Status)
-
-		if err != nil {
-			if err == sql.ErrNoRows {
-				return nil, errs.NewNotFoundError("Customers not found")
-			} else {
-				logger.Error("Error while scanning customers" + err.Error())
-				return nil, errs.NewUnexpectedError("unexpected database error")
-			}
+		if err == sql.ErrNoRows {
+			return nil, errs.NewNotFoundError("Customer not found")
+		} else {
+			logger.Error("Error while scanning customer" + err.Error())
+			return nil, errs.NewUnexpectedError("unexpected database error")
 		}
-		customers = append(customers, &c)
 	}
 
 	return customers, nil
-
 }
 
 func (r *CustomerRepoDB) ById(id string) (*Customer, *errs.AppError) {
-	findCustomerByIdSqlQuery := "SELECT customer_id, name, city, zipcode, date_of_birth, status FROM customers WHERE customer_id = ?"
-
-	row := r.db.Raw(findCustomerByIdSqlQuery, id).Row()
+	findCustomerByIdSqlQuery := "SELECT customer_id, name, city, zipcode, date_of_birth, status FROM customers WHERE customer_id = $1"
 
 	var c Customer
-
-	err := row.Scan(&c.ID, &c.Name, &c.City, &c.Zipcode, &c.DateOfBirth, &c.Status)
+	err := r.db.Get(&c, findCustomerByIdSqlQuery, id)
 
 	if err != nil {
 		if err == sql.ErrNoRows {
