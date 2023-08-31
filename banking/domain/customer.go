bea6c24190227087f2dd2c70e99f82058f791e84
H-proshanto <proshantolal3@gmail.com>
2023-08-31 23:54:52 +0600

    adds DTO for customer response

--- banking/domain/customer.go
+++ banking/domain/customer.go
@@ -1,6 +1,9 @@
 package domain
 
-import "github.com/H-proshanto/go-banking-microservice/banking/errs"
+import (
+	"github.com/H-proshanto/go-banking-microservice/banking/dto"
+	"github.com/H-proshanto/go-banking-microservice/banking/errs"
+)
 
 type Customer struct {
 	ID          string `db:"customer_id"`
@@ -9,6 +12,29 @@ type Customer struct {
 	Zipcode     string
 	DateOfBirth string `db:"date_of_birth"`
 	Status      string
+}
+
+func (c *Customer) statusAsText() string {
+	statusAsText := "active"
+
+	if c.Status == "0" {
+		statusAsText = "inactive"
+	}
+	
+	return statusAsText
+}
+
+func (c *Customer) ToResponseDTO() *dto.CustomerResponse {
+
+
+	return &dto.CustomerResponse{
+		ID:          c.ID,
+		Name:        c.Name,
+		City:        c.City,
+		Zipcode:     c.Zipcode,
+		DateOfBirth: c.DateOfBirth,
+		Status:      c.statusAsText(),
+	}
 }
 
 type CustomerRepository interface {
--- banking/service/customerSvc.go
+++ banking/service/customerSvc.go
@@ -2,19 +2,20 @@ package service
 
 import (
 	"github.com/H-proshanto/go-banking-microservice/banking/domain"
+	"github.com/H-proshanto/go-banking-microservice/banking/dto"
 	"github.com/H-proshanto/go-banking-microservice/banking/errs"
 )
 
 type CustomerService interface {
-	GetAllCustomers(status string) ([]*domain.Customer, *errs.AppError)
-	GetCustomer(id string) (*domain.Customer, *errs.AppError)
+	GetAllCustomers(status string) ([]*dto.CustomerResponse, *errs.AppError)
+	GetCustomer(id string) (*dto.CustomerResponse, *errs.AppError)
 }
 
 type DefaultCustomerService struct {
 	repo domain.CustomerRepository
 }
 
-func (s DefaultCustomerService) GetAllCustomers(status string) ([]*domain.Customer, *errs.AppError) {
+func (s DefaultCustomerService) GetAllCustomers(status string) ([]*dto.CustomerResponse, *errs.AppError) {
 	if status == "active" {
 		status = "1"
 	} else if status == "inactive" {
@@ -23,11 +24,28 @@ func (s DefaultCustomerService) GetAllCustomers(status string) ([]*domain.Custom
 		status = ""
 	}
 
-	return s.repo.FindAll(status)
+	customers, err := s.repo.FindAll(status)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var response []*dto.CustomerResponse
+	for _, customer := range customers {
+		response = append(response, customer.ToResponseDTO())
+	}
+
+	return response, nil
 }
 
-func (s DefaultCustomerService) GetCustomer(id string) (*domain.Customer, *errs.AppError) {
-	return s.repo.ById(id)
+func (s DefaultCustomerService) GetCustomer(id string) (*dto.CustomerResponse, *errs.AppError) {
+	customer, err := s.repo.ById(id)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return customer.ToResponseDTO(), nil
 }
 
 func NewCustomerService(repo domain.CustomerRepository) *DefaultCustomerService {
